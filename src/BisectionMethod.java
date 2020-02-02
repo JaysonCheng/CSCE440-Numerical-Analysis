88548a4708245df4ef22bbfb24c8a3bae1afcd82
JaysonCheng <57777740+JaysonCheng@users.noreply.github.com>
2020-02-02 16:08:09 -0600

    Assignment1: Added other methods to find roots for equation

--- src/BisectionMethod.java
+++ src/BisectionMethod.java
@@ -1,40 +1,74 @@
+// Name: Jayson Cheng
+// CSCE440 Assignment 1
 
 public class BisectionMethod {
 	
+	// error tolerance level
 	static final float tolerance = (float) 0.0001;
 	
-	static double function(double x) {
-		return 2*Math.pow(x, 3) - Math.pow(x, 2) + 6*x - Math.exp(x) + 2;
+	// maximum number of iterations
+	static final int maxNumOfIterations = 50;
+		
+	// f(x) for part a
+	static double function1(double x) {
+		return 2 * Math.pow(x, 3) - Math.pow(x, 2) + 6*x - Math.exp(x) + 2;
 	}
 	
-	static void bisection(double a, double b, double maxNumOfIterations) {
+	// f(x) for part b
+	static double function2(double x) {
+		return 3 * Math.pow(x, 4) + Math.pow(x, 2) - 2;
+//		return 3 * x - 3 * Math.pow(x, 2) + 2 * Math.exp(x) - 2;
+	}
+		
+	// f(x) for part d
+	static double function3(double x) {
+		return 2 * x - Math.cos(x);
+	}
+	
+	// bisection method
+	static void bisection(double a, double b) {
+		
 		int i = 1;
-		double f1 = function(a);
-		double f2 = function(b);
-		double f3;
+		double f1 = function2(a);
+		double f2;
 		double midpoint;
 		double c;
+		
+		System.out.println("Bisection Method : ");
+		System.out.println("Iteration \t" + "Results");
+		System.out.println("======================================");
+
 		while(i <= maxNumOfIterations) {
+			// compute pi
 			midpoint = a + ((b - a) / 2);
-			f3 = function(midpoint);
+			f2 = function2(midpoint);
+
+			// print out results for each iteration
+			System.out.println(i + "\t\t" + midpoint);
+	
 			c = (b - a) / 2;
-			if(f3 == 0 || c < tolerance) {
-				System.out.println("The root is " + midpoint);
+			
+			// condition to determine if the procedure is successful
+			if(f2 == 0 || c < tolerance) {
 				return;
 			}
+			
 			i++;
-			if(f1 * f3 > 0 ) {
+			
+			// compute ai, bi
+			if(f1 * f2 > 0 ) {
 				a = midpoint;
-				f1 = f3;
+				f1 = f2;
 			} else {
 				b = midpoint;
-				f2 = f3;
 			}
 		}
+		
+		// The method failed after 50 iterations
 		System.out.println("The procedure was unsuccessful.");
 	}
 	
 	public static void main(String[] args) {
-		bisection(-1,1,50);
+		bisection(-3,1);
 	}
 }
