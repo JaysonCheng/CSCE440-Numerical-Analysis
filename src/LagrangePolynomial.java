1b00e445a06b010dbdedb1b5579f0734e3c851bd
JaysonCheng <57777740+JaysonCheng@users.noreply.github.com>
2020-02-26 22:11:08 -0600

    added CubicSplineMethod

--- src/LagrangePolynomial.java
+++ src/LagrangePolynomial.java
@@ -2,7 +2,8 @@
 // CSCE440 Assignment 2
 
 public class LagrangePolynomial {
-
+	
+	// Calculate the lagrange polynomial for each k=0,1,....,n
 	static double lagrange(double x, int point, int[] start) {
 		double result = 1;
 		for(int i = 0; i < start.length; i ++) {
@@ -13,15 +14,7 @@ public class LagrangePolynomial {
 		return result;
 	}
 	
-	static int getIndex(int point, int[] start) {
-		for(int i = 0; i < start.length; i++) {
-			if(start[i] == point) {
-				return i;
-			}
-		}
-		return 0;
-	}
-	
+	// Calculate the value of the interpolating polynomial
 	static double polynomial(double x, int[] start, int[] end) {
 		double sum = 0;
 		double result = 0;
@@ -33,10 +26,46 @@ public class LagrangePolynomial {
 	}
 	
 	public static void main(String args[]) {
-		int[] start = new int[] {12, 15, 19, 22};
-		int[] end = new int[] {27, 29, 32, 35};
-		double result = polynomial(17, start, end);
-		System.out.println(result);
-
+		//station 1
+		int[] start1 = new int[] {1, 5, 8, 12, 15, 19, 22, 26, 29};
+		int[] end1 = new int[] {30, 33, 35, 27, 29, 32, 35, 37, 39};
+		double result1 = polynomial(17, start1, end1);
+		System.out.print("PM 2.5 for weather station 1: ");
+		System.out.println(result1);
+		
+		//station 2
+		int[] start2 = new int[] {2, 4, 9, 11, 16, 18, 23, 25, 30};
+		int[] end2 = new int[] {36, 35, 30, 28, 34, 32, 36, 37, 40};
+		double result2 = polynomial(17, start2, end2);
+		System.out.print("PM 2.5 for weather station 2: ");
+		System.out.println(result2);
+		
+		//station 3
+		int[] start3 = new int[] {6, 13, 20, 27};
+		int[] end3 = new int[] {42, 36, 38, 40};
+		double result3 = polynomial(17, start3, end3);
+		System.out.print("PM 2.5 for weather station 3: ");
+		System.out.println(result3);
+		
+		//station 4
+		int[] start4 = new int[] {7, 14, 21, 28};
+		int[] end4 = new int[] {32, 34, 36, 35};
+		double result4 = polynomial(17, start4, end4);
+		System.out.print("PM 2.5 for weather station 4: ");
+		System.out.println(result4);
+		
+		//station 5
+		int[] start5 = new int[] {5, 10, 15, 20};
+		int[] end5 = new int[] {28, 30, 33, 31};
+		double result5 = polynomial(17, start5, end5);
+		System.out.print("PM 2.5 for weather station 5: ");
+		System.out.println(result5);
+		
+		//station 6
+		int[] start6 = new int[] {8, 15, 22, 29};
+		int[] end6 = new int[] {30, 37, 42, 44};
+		double result6 = polynomial(17, start6, end6);
+		System.out.print("PM 2.5 for weather station 6: ");
+		System.out.println(result6);
 	}
 }
--- src/NevillesMethod.java
+++ src/NevillesMethod.java
@@ -3,6 +3,7 @@
 
 public class NevillesMethod {
 	
+	// Construct a recursive table by applying Nevilles Method
 	static double[][] Neville(int[] x, int[] f, double point){
 		int n = x.length;
 		double[][] q = new double[n][n];
@@ -26,16 +27,99 @@ public class NevillesMethod {
 		System.out.println("Nevilles Method : ");
 		System.out.println("=========================================================================");
 		
-		int[] x = new int[] {1, 5 ,8, 12, 15};
-		int[] f = new int[] {30, 33 , 35, 27, 29};
-		double[][] q = Neville(x, f, 10);
+		// Station 1
+		System.out.println("Station 1 : ");
+		System.out.println("=========================================================================");
 		
-		for(int i = 0; i < x.length; i++) {
-			for(int j = 0; j < f.length; j++) {
-				System.out.printf("%.5f \t", q[i][j]);
+		int[] start1 = new int[] {1, 5, 8, 12, 15, 19, 22, 26, 29};
+		int[] end1 = new int[] {30, 33, 35, 27, 29, 32, 35, 37, 39};
+		double[][] result1 = Neville(start1, end1, 11);
+		
+		for(int i = 0; i < start1.length; i++) {
+			for(int j = 0; j < end1.length; j++) {
+				System.out.printf("%.5f \t", result1[i][j]);
 			}
 			System.out.println();
 		}
-
+		
+		// Station 2
+		System.out.println();
+		System.out.println("Station 2 : ");
+		System.out.println("=========================================================================");
+		
+		int[] start2 = new int[] {2, 4, 9, 11, 16, 18, 23, 25, 30};
+		int[] end2 = new int[] {36, 35, 30, 28, 34, 32, 36, 37, 40};
+		double[][] result2 = Neville(start2, end2, 11);
+		
+		for(int i = 0; i < start2.length; i++) {
+			for(int j = 0; j < end2.length; j++) {
+				System.out.printf("%.5f \t", result2[i][j]);
+			}
+			System.out.println();
+		}
+		
+		// Station 3
+		System.out.println();
+		System.out.println("Station 3 : ");
+		System.out.println("=========================================================================");
+		
+		int[] start3 = new int[] {6, 13, 20, 27};
+		int[] end3 = new int[] {42, 36, 38, 40};
+		double[][] result3 = Neville(start3, end3, 11);
+		
+		for(int i = 0; i < start3.length; i++) {
+			for(int j = 0; j < end3.length; j++) {
+				System.out.printf("%.5f \t", result3[i][j]);
+			}
+			System.out.println();
+		}
+		
+		// Station 4
+		System.out.println();
+		System.out.println("Station 4 : ");
+		System.out.println("=========================================================================");
+		
+		int[] start4 = new int[] {7, 14, 21, 28};
+		int[] end4 = new int[] {32, 34, 36, 35};
+		double[][] result4 = Neville(start4, end4, 11);
+		
+		for(int i = 0; i < start4.length; i++) {
+			for(int j = 0; j < end4.length; j++) {
+				System.out.printf("%.5f \t", result4[i][j]);
+			}
+			System.out.println();
+		}
+		
+		// Station 5
+		System.out.println();
+		System.out.println("Station 5 : ");
+		System.out.println("=========================================================================");
+		
+		int[] start5 = new int[] {5, 10, 15, 20};
+		int[] end5 = new int[] {28, 30, 33, 31};
+		double[][] result5 = Neville(start5, end5, 11);
+		
+		for(int i = 0; i < start5.length; i++) {
+			for(int j = 0; j < end5.length; j++) {
+				System.out.printf("%.5f \t", result5[i][j]);
+			}
+			System.out.println();
+		}
+		
+		// Station 6
+		System.out.println();
+		System.out.println("Station 6 : ");
+		System.out.println("=========================================================================");
+		
+		int[] start6 = new int[] {8, 15, 22, 29};
+		int[] end6 = new int[] {30, 37, 42, 44};
+		double[][] result6 = Neville(start6, end6, 11);
+		
+		for(int i = 0; i < start6.length; i++) {
+			for(int j = 0; j < end6.length; j++) {
+				System.out.printf("%.5f \t", result6[i][j]);
+			}
+			System.out.println();
+		}
 	}
 }
